7ed594627b73aa893096aed8ffd78bf3b4032a87
Kaloyan Stefanov <KaloyanchoSt@gmail.com>
2016-11-03 14:43:33 +0200

    Add files via upload

---
+++ 1.Intro-Java-Homework/src/AssignVariables.java
@@ -0,0 +1,16 @@
+/**
+ * Created by kokolaptop on 10/10/2016.
+ */
+public class AssignVariables {
+    public static void main(String[] args) {
+        byte a = 127;
+        short b = 32767;
+        int c = 2000000000;
+        long d = 919827112351L;
+        char e = 'c';
+        boolean bool =  false;
+        float f = 0.5f;
+        double g = 0.1234567891011;
+        String firstName = "Palo Alto, CA";
+    }
+}
---
+++ 1.Intro-Java-Homework/src/PrintCharacters.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kokolaptop on 10/10/2016.
+ */
+public class PrintCharacters {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            System.out.print(c + " ");
+        }
+    }
+}
